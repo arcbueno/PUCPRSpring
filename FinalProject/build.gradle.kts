62355a7ee9ebe2a9d4a5e226d20e43ba604a164b
Pedro Bueno <pedroarcbueno@gmail.com>
2025-10-22 21:03:55 -0300

    New authserver project

--- FinalProject/build.gradle.kts
+++ FinalProject/build.gradle.kts
@@ -51,3 +51,17 @@ allOpen {
 	annotation("jakarta.persistence.MappedSuperclass")
 	annotation("jakarta.persistence.Embeddable")
 }
+
+tasks.withType<Jar> {
+
+	// To avoid the duplicate handling strategy error
+	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+	// To add all of the dependencies
+	from(sourceSets.main.get().output)
+
+	dependsOn(configurations.runtimeClasspath)
+	from({
+		configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+	})
+}
